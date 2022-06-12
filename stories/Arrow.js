4441a0fc29d78eb6cd944e657adbf160128ed439
Jessim Heddadi <jessim.heddadi@influence4you.com>
2022-06-12 12:52:52 +0200

    clean code indentation test

--- stories/Arrow.js
+++ stories/Arrow.js
@@ -2,15 +2,14 @@ import '../assets/styles/styles.css';
 import arrow from '../assets/images/arrow.svg';
 
 
-export const createIcon = ({
-}) => {
+export const createIcon = ({}) => {
 
-  const arrowIcon = {
-    src: arrow,
-  };
-  const svg = document.createElement('img')
+    const arrowIcon = {
+        src: arrow,
+    };
+    const svg = document.createElement('img')
 
-  svg.src = arrowIcon.src;
+    svg.src = arrowIcon.src;
 
-  return svg
+    return svg
 }
